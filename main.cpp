481cd0a393e248174c8932fe48966727fddeb991
mMichele-github <100371043+mMichele-github@users.noreply.github.com>
2022-04-02 10:38:46 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    std::cout << "Hello, World!" << std::endl;
+    float a=0, b=0, x=0, y=0, z=0;
+    cout << "Inserire quattro numeri reali a piacere a, b, x, y :"<< endl;
+    cin >> a >> b >> x >> y;
+    if (x<0 && y>0)
+    {
+        z = a * x - b * y;
+        cout << "z = " << z << endl;
+    }
+    else if(x>=0 && y<=0)
+    {
+        z = a*x*x - b*y;
+        cout  <<"z = "<< z<< endl;
+    }
+    else
+    {
+        z=a*x + b*y*y;
+        cout << "z = " << z <<endl;
+
+    }
+    return 0;
 }
